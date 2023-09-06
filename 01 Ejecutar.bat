fa4b45557bfd23f38c17bf565a1f99f94594a6c2
Andrés García <andresgarcia0313@gmail.com>
2023-09-06 01:30:53 -0500

    Se agrega mensaje al desarrollador de esperar o validar puerto en .env

--- 01 Ejecutar.bat
+++ 01 Ejecutar.bat
@@ -1,3 +1,7 @@
 docker compose up -d
+@REM Borrado de pantalla
+cls
+@REM Mensaje en bat al usuario en letras en rojo indicando que el navegador abrira y que lo refresque hasta cargar la pagina
+echo "Abriendo navegador, refresque hasta que cargue la pagina si no funciona valide el puerto que debes colocar en .env"
 start http://localhost
 pause
